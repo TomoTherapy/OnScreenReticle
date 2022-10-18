97abb805859a53a5fd000c3d4178b4f65f9ff9f0
홍준기 <wnsrlghd@gmail.com>
2022-10-18 18:01:59 +0900

    xbox widget add

---
+++ OnScreenReticle/OnScreenReticleXboxGameBar/JsonParser.cs
@@ -0,0 +1,122 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnScreenReticleXboxGameBar
+{
+    internal class JsonParser
+    {
+        Settings settings;
+        JsonSerializer serializer;
+
+        public JsonParser()
+        {
+            DeserializeSettings();
+        }
+
+        public void SerializeSettings()
+        {
+            serializer.NullValueHandling = NullValueHandling.Ignore;
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\OnScreenReticle\OnScreenReticle.json"; ;
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                serializer.Serialize(sw, settings);
+            }
+        }
+
+        public void DeserializeSettings()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\OnScreenReticle";
+            Directory.CreateDirectory(path);
+            string fullPath = Path.Combine(path, "OnScreenReticle.json");
+
+            if (File.Exists(fullPath))
+            {
+                string json = "";
+                using (StreamReader sr = new StreamReader(fullPath))
+                {
+                    json = sr.ReadToEnd();
+                }
+
+                settings = JsonConvert.DeserializeObject<Settings>(json);
+            }
+
+            if (settings == null)
+            {
+                settings = new Settings();
+            }
+        }
+    }
+
+    public class Settings
+    {
+        public Settings()
+        {
+            Top = 250;
+            Left = 125;
+
+            DotDiameter = 5;
+            DotColorA = 255;
+            DotColorR = 255;
+            DotColorG = 0;
+            DotColorB = 0;
+            DotVisibility = true;
+
+            AngleThickness = 5;
+            AngleLength = 30;
+            AngleAngle = 90;
+            AngleColorA = 255;
+            AngleColorR = 255;
+            AngleColorG = 0;
+            AngleColorB = 0;
+            AngleVisibility = true;
+
+            CrossThickness = 5;
+            CrossLength = 30;
+            CrossOffset = 15;
+            CrossAngle = 0;
+            CrossColorA = 255;
+            CrossColorR = 255;
+            CrossColorG = 0;
+            CrossColorB = 0;
+            CrossVisibility = true; 
+        }
+
+        public int Top { get; set; }
+        public int Left { get; set; }
+
+        // Dot
+        public double DotDiameter { get; set; }
+        public int DotColorA { get; set; }
+        public int DotColorR { get; set; }
+        public int DotColorG { get; set; }
+        public int DotColorB { get; set; }
+        public bool DotVisibility { get; set; }
+
+        // Angle
+        public double AngleThickness { get; set; }
+        public double AngleLength { get; set; }
+        public double AngleAngle { get; set; }
+        public int AngleColorA { get; set; }
+        public int AngleColorR { get; set; }
+        public int AngleColorG { get; set; }
+        public int AngleColorB { get; set; }
+        public bool AngleVisibility { get; set; }
+
+        // Cross
+        public double CrossThickness { get; set; }
+        public double CrossLength { get; set; }
+        public double CrossOffset { get; set; }
+        public double CrossRotation { get; set; }
+        public int CrossColorA { get; set; }
+        public int CrossColorR { get; set; }
+        public int CrossColorG { get; set; }
+        public int CrossColorB { get; set; }
+        public bool CrossVisibility { get; set; }
+    }
+}
---
+++ OnScreenReticle/OnScreenReticleXboxGameBar/Reticle.xaml.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Gaming.XboxGameBar;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace OnScreenReticleXboxGameBar
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Reticle : Page, INotifyPropertyChanged
+    {
+        #region Property Changed
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+
+        private XboxGameBarWidget widget;
+        private Settings settings;
+
+        // Location
+        public int Top { get => settings.Top; set { settings.Top = value; NotifyPropertyChanged(); NotifyPropertyChanged(nameof(MarginString)); } }
+        public int Left { get => settings.Left; set { settings.Left = value; NotifyPropertyChanged(); NotifyPropertyChanged(nameof(MarginString)); } }
+        public string MarginString { get => $"{Left},{Top},0,0"; }
+
+        // Dot
+        public double DotDiameter { get => settings.DotDiameter; set { settings.DotDiameter = value; } }
+        public int DotColorA { get => settings.DotColorA; set { settings.DotColorA = value; } }
+        public int DotColorR { get => settings.DotColorR; set { settings.DotColorR = value; } }
+        public int DotColorG { get => settings.DotColorG; set { settings.DotColorG = value; } }
+        public int DotColorB { get => settings.DotColorB; set { settings.DotColorB = value; } }
+        public string DotColorString { get => $"#{DotColorA.ToString("X2")}{DotColorR.ToString("X2")}{DotColorG.ToString("X2")}{DotColorB.ToString("X2")}"; }
+        public bool DotVisibility { get => settings.DotVisibility; set { settings.DotVisibility = value; } }
+        public string DotVisibilityString { get => DotVisibility ? "Visible" : "Collapsed"; }
+
+        // Angle
+        public double AngleThickness { get => settings.AngleThickness; set { settings.AngleThickness = value; } }
+        public double AngleLength { get => settings.AngleLength; set { settings.AngleLength = value; } }
+        public double AngleAngle { get => settings.AngleAngle; set { settings.AngleAngle = value; } }
+        public int AngleColorA { get => settings.AngleColorA; set { settings.AngleColorA = value; } }
+        public int AngleColorR { get => settings.AngleColorR; set { settings.AngleColorR = value; } }
+        public int AngleColorG { get => settings.AngleColorG; set { settings.AngleColorG = value; } }
+        public int AngleColorB { get => settings.AngleColorB; set { settings.AngleColorB = value; } }
+        public string AngleColorString { get => $"#{AngleColorA.ToString("X2")}{AngleColorR.ToString("X2")}{AngleColorG.ToString("X2")}{AngleColorB.ToString("X2")}"; }
+        public bool AngleVisibility { get => settings.AngleVisibility; set { settings.AngleVisibility = value; } }
+        public string AngleVisibilityString { get => AngleVisibility ? "Visible" : "Collapsed"; }
+
+        // Cross
+        public double CrossThickness { get => settings.CrossThickness; set { settings.CrossThickness = value; } }
+        public double CrossLength { get => settings.CrossLength; set { settings.CrossLength = value; } }
+        public double CrossOffset { get => settings.CrossOffset; set { settings.CrossOffset = value; } }
+        public double CrossRotation { get => settings.CrossRotation; set { settings.CrossRotation = value; } }
+        public int CrossColorA { get => settings.CrossColorA; set { settings.CrossColorA = value; } }
+        public int CrossColorR { get => settings.CrossColorR; set { settings.CrossColorR = value; } }
+        public int CrossColorG { get => settings.CrossColorG; set { settings.CrossColorG = value; } }
+        public int CrossColorB { get => settings.CrossColorB; set { settings.CrossColorB = value; } }
+        public string CrossColorString { get => $"#{CrossColorA.ToString("X2")}{CrossColorR.ToString("X2")}{CrossColorG.ToString("X2")}{CrossColorB.ToString("X2")}"; }
+        public bool CrossVisibility { get => settings.CrossVisibility; set { settings.CrossVisibility = value; } }
+        public string CrossVisibilityString { get => CrossVisibility ? "Visible" : "Collapsed"; }
+
+
+        public Reticle()
+        {
+            this.InitializeComponent();
+            settings = ((App)Application.Current).Settings;
+
+            DataContext = this;
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            widget = e.Parameter as XboxGameBarWidget;
+
+            widget.SettingsClicked += Widget_SettingsClicked;
+
+            await widget.CenterWindowAsync();
+        }
+
+        private async void Widget_SettingsClicked(XboxGameBarWidget sender, object args)
+        {
+            await sender.ActivateSettingsAsync();
+
+            await sender.CenterWindowAsync();
+        }
+    }
+}
